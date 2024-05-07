fd277032d771b4dc5a4209a3dd1a92236d5048a3
Rohit Sav <rohitsav1305@gmail.com>
2024-05-07 19:53:33 +0530

    Product and Category Schema Created

--- product/models.py
+++ product/models.py
@@ -1,40 +1,48 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.utils import timezone
+
 
 class Category(models.Model):
-    name = models.CharField(max_length=255)
-    
-    def __str__(self):
-        return self.name 
+    category_id = models.AutoField(primary_key=True)
+    category_name = models.CharField(max_length=255)
+    category_is_active = models.BooleanField(default=True)
+    category_updated_date = models.DateTimeField(default=timezone.now)
+    category_created_date = models.DateTimeField(default=timezone.now)
 
-class Product(models.Model):
-    SALE = 'SALE'
-    NEW = 'NEW'
-    HOT = 'HOT'
-    DISCOUNT = 'DISCOUNT'
-    TAG_CHOICES = [
-        (SALE, 'Sale'),
-        (NEW, 'New'),
-        (HOT, 'Hot'),
-        (DISCOUNT, 'Discount'),
-    ]
-    
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    tag = models.CharField(max_length=255, choices=TAG_CHOICES, default=NEW)
-    title = models.CharField(max_length=255)
-    rating = models.DecimalField(max_digits=3, decimal_places=2, default=0)
-    seller = models.CharField(max_length=255)
-    original_amount = models.DecimalField(max_digits=10, decimal_places=2)
-    discounted_amount = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='products/')
-    description = models.TextField()
-    size_or_weight = models.CharField(max_length=255)
-    quantity = models.IntegerField()
-    product_type = models.CharField(max_length=255)
-    manufacturing_date = models.DateField()
-    lifespan = models.IntegerField()
-    stocks_left = models.IntegerField()
-    tags = models.CharField(max_length=255)
-    
     def __str__(self):
-        return self.title
+        return self.name
+class Product(models.Model):
+    product_id = models.AutoField(primary_key=True)
+    product_name = models.CharField(max_length=255)
+    product_is_active = models.BooleanField(default=True)
+    product_created_date = models.DateTimeField(detault=timezone.now)
+    product_updated_date = models.DateTimeField(default=timezone.now)
+
+    sale = 'SALE'
+    new = 'NEW'
+    hot = 'HOT'
+    discount = 'DISCOUNT'
+    product_tag_choice = [
+        (sale, 'Sale'),
+        (new, 'New'),
+        (hot, 'Hot'),
+        (discount, 'Discount'),
+    ]
+
+    product_category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, db_column='category_id')
+    product_tag = models.CharField(
+        max_length=255, choices=product_tag_choice, default=new)
+    product_title = models.CharField(max_length=255)
+    product_rating = models.DecimalField(
+        max_digits=3, decimal_places=2, default=0)
+    product_seller_name = models.CharField(max_length=255)
+    product_price = models.DecimalField(max_digits=10, decimal_places=2)
+    product_image = models.ImageField(upload_to='products/')
+    product_description = models.TextField()
+    product_manufacturing_date = models.DateField()
+    product_lifespan = models.IntegerField()
+    product_stocks_left = models.IntegerField()
+
+    def __str__(self):
+        return self.product_name
