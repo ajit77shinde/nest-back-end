2695988b57f5528c7f71559b7694fd1245d67833
Rohit Sav <rohitsav1305@gmail.com>
2024-05-08 12:57:41 +0530

    added extra field in products models file for tracking

--- product/models.py
+++ product/models.py
@@ -5,18 +5,21 @@ from django.utils import timezone
 class Category(models.Model):
     category_id = models.AutoField(primary_key=True)
     category_name = models.CharField(max_length=255)
+
+    # extra fields for tracking
     category_is_active = models.BooleanField(default=True)
-    category_updated_date = models.DateTimeField(default=timezone.now)
-    category_created_date = models.DateTimeField(default=timezone.now)
+    category_created_by = models.CharField(max_length=255)
+    category_updated_by = models.CharField(max_length=255)
+    category_created_timestamp = models.DateField(default=timezone.now)
+    category_updated_timestamp = models.DateField(default=timezone.now)
 
     def __str__(self):
-        return self.name
+        return self.category_name
+
+
 class Product(models.Model):
     product_id = models.AutoField(primary_key=True)
     product_name = models.CharField(max_length=255)
-    product_is_active = models.BooleanField(default=True)
-    product_created_date = models.DateTimeField(detault=timezone.now)
-    product_updated_date = models.DateTimeField(default=timezone.now)
 
     sale = 'SALE'
     new = 'NEW'
@@ -34,8 +37,6 @@ class Product(models.Model):
     product_tag = models.CharField(
         max_length=255, choices=product_tag_choice, default=new)
     product_title = models.CharField(max_length=255)
-    product_rating = models.DecimalField(
-        max_digits=3, decimal_places=2, default=0)
     product_seller_name = models.CharField(max_length=255)
     product_price = models.DecimalField(max_digits=10, decimal_places=2)
     product_image = models.ImageField(upload_to='products/')
@@ -43,6 +44,13 @@ class Product(models.Model):
     product_manufacturing_date = models.DateField()
     product_lifespan = models.IntegerField()
     product_stocks_left = models.IntegerField()
+
+    # extra fields for tracking
+    category_is_active = models.BooleanField(default=True)
+    category_created_by = models.CharField(max_length=255)
+    category_updated_by = models.CharField(max_length=255)
+    category_created_timestamp = models.DateField(default=timezone.now)
+    category_updated_timestamp = models.DateField(default=timezone.now)
 
     def __str__(self):
         return self.product_name
